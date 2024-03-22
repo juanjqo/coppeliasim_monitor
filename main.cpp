b99b72c6bec0d71139a8ba72d9383d6e22739b28
Juancho <juanjqogm@gmail.com>
2024-03-22 15:10:15 -0500

    updated the submodule

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
+
 #include "imgui_coppeliasim_interface.h"
-#include <vector>
+
 
 using namespace std;
 
@@ -11,18 +11,13 @@ int main()
     parameters.height = 1080;
     parameters.title = "CoppeliaSim Commander by Juancho";
     parameters.screen_mode = "LIGHT_MODE";
-    parameters.font_path = FONT_RESOURCE_DIR;
+    parameters.font_path = "default";
     parameters.font_size = 25;
 
     ImguiCoppeliaSimInterface imgui_coppeliasim_interface(parameters);
     imgui_coppeliasim_interface.set_capi_path(CAPI_RESOURCE_DIR);
 
     imgui_coppeliasim_interface.loop();
-
-    imgui_coppeliasim_interface.cleanup();
-
-
-
 
     return 0;
 }
